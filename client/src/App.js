7c72d9fef2166aa657e257b593ea64d8f188b3e3
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-08-25 17:27:59 +0300

    App now with buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,158 +1,95 @@
-import { useState, useEffect } from 'react'
-import nameService from './services/phonebook'
+import { useState } from 'react'
+import Phonebook from './components/Phonebook'
 
-const Notification = ({ message, errorState }) => {
+const MainContainer = ({ windowState }) => {
 
-	let notificationStyle = {
-		color: 'green',
-		background: 'lightgrey',
-		fontSize: 20,
-		borderStyle: 'solid',
-		borderRadius: 5,
-		padding: 10,
-		marginBottom: 10,
+	const submitUser = (event) => {
+		event.preventDefault()
+		console.log("Sending user data! Or not really...")
+
+		// const oldNumber = persons.find(person => person.name === newName)
+		// if (oldNumber) {
+		// 	if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+		// 		const changedNumber = { ...oldNumber, number: newNumber }
+		// 		nameService
+		// 			.update(oldNumber.id, changedNumber)
+		// 			.then(returnedName => {
+		// 				setPersons(persons.map(person => person.name !== newName ? person : returnedName))
+		// 			})
+		// 			.catch(error => {
+		// 				showMessage(`Information of '${newName}' has already been removed from server`, 'error')
+		// 				setPersons(persons.filter(person => person.name !== newName))
+		// 			})
+		// 		showMessage(`Changed ${newName}'s number`, 'success')
+		// 	}
+		// } else {
+		// 	const nameObject = { name: newName, number: newNumber }
+		// 	nameService
+		// 		.create(nameObject)
+		// 		.then(returnedName => {
+		// 			setPersons(persons.concat(returnedName))
+		// 		})
+		// 	showMessage(`Added ${newName}`, 'success')
+		// }
 	}
 
-	if (errorState) {
-		notificationStyle.color = 'red'
+	if (windowState === 'signup') {
+		return (
+			<>
+				<h2>{windowState}</h2>
+				<form onSubmit={submitUser}>
+					<br></br>
+					<div><input type="text" name="username" defaultValue="Username"></input></div>
+					<div><input type="text" name="firstname" defaultValue="First name"></input></div>
+					<div><input type="text" name="lastname" defaultValue="Last name"></input></div>
+					<div><input type="text" name="password" defaultValue="Password"></input></div>
+					<div><input type="text" name="confirm_password" defaultValue="Confirm password"></input></div>
+					<button type="submit">Submit</button>
+				</form>
+			</>
+		)
 	}
-
-	if (message === null) {
-		return null
-	}
-
 	return (
-		<div style={notificationStyle}>
-			{message}
-		</div>
+		<h2>{windowState}</h2>
 	)
 }
 
-const Filter = ({ value, onChange }) =>
-	<div>filter shown with <input value={value} onChange={onChange} /></div>
-
-const PersonForm = ({ addName, newName, onNameChange, newNumber, onNumberChange }) => {
-	return (
-		<form onSubmit={addName}>
-			<div>name: <input value={newName} onChange={onNameChange} /></div>
-			<div>number: <input value={newNumber} onChange={onNumberChange} /></div>
-			<div><button type="submit">add</button></div>
-		</form>
-	)
-}
-
-const Person = ({ name, number, id, setPersons, showMessage }) => {
-
-	const deleteNote = id => {
-		if (window.confirm(`Delete ${name}?`)) {
-			nameService.remove(id)
-				.then(() =>
-					nameService.getAll()
-						.then(newNames => {
-							setPersons(newNames)
-							showMessage(`Deleted ${name}`, 'success')
-						}))
-		}
-	}
-
-	return (
-		<p>{name} {number} <button onClick={() => deleteNote(id)}>delete</button></p>
-	)
-}
-
-const Persons = ({ persons, setPersons, showMessage }) => {
+const Buttons = ({ setWindowState }) => {
 	return (
 		<>
-			{persons.map(person =>
-				<Person key={person.name} name={person.name} number={person.number} id={person.id}
-					setPersons={setPersons} showMessage={showMessage} />
-			)}
+			<button onClick={() => setWindowState('login')}>Log in</button>
+			<button onClick={() => setWindowState('signup')}>Sign up</button>
+			<button onClick={() => setWindowState('profile')}>Profile</button>
+			<button onClick={() => setWindowState('browse_users')}>Browse users</button>
+			<button onClick={() => setWindowState('browse_users')}>Chat</button>
+			<button onClick={() => setWindowState('phonebook')}>Phonebook</button>
 		</>
 	)
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([])
-	const [newName, setNewName] = useState('')
-	const [newNumber, setNewNumber] = useState('')
-	const [newFilter, setNewFilter] = useState('')
-	const [message, setMessage] = useState(null)
-	const [errorState, setErrorState] = useState(false)
+	const [windowState, setWindowState] = useState('phonebook')
 
-	useEffect(() => {
-		nameService
-			.getAll()
-			.then(initialNames => {
-				setPersons(initialNames)
-			})
-	}, [])
-
-	const showMessage = (content, style) => {
-		if (style === 'error') {
-			setErrorState(true)
-		}
-		setMessage(`${content}`)
-		setTimeout(() => {
-			setMessage(null)
-			setErrorState(false)
-		}, 4000)
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+		  console.log(position.coords.latitude);
+		  console.log(position.coords.longitude);
+		});
 	}
 
-	const addName = (event) => {
-		event.preventDefault()
-
-		const oldNumber = persons.find(person => person.name === newName)
-		if (oldNumber) {
-			if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-				const changedNumber = { ...oldNumber, number: newNumber }
-				nameService
-					.update(oldNumber.id, changedNumber)
-					.then(returnedName => {
-						setPersons(persons.map(person => person.name !== newName ? person : returnedName))
-					})
-					.catch(error => {
-						showMessage(`Information of '${newName}' has already been removed from server`, 'error')
-						setPersons(persons.filter(person => person.name !== newName))
-					})
-				showMessage(`Changed ${newName}'s number`, 'success')
-			}
-		} else {
-			const nameObject = { name: newName, number: newNumber }
-			nameService
-				.create(nameObject)
-				.then(returnedName => {
-					setPersons(persons.concat(returnedName))
-				})
-			showMessage(`Added ${newName}`, 'success')
-		}
+	if (windowState === 'phonebook') {
+		return (
+			<div>
+				<Buttons setWindowState={setWindowState} />
+				<Phonebook />
+			</div>
+		)
 	}
-
-	const handleNameChange = (event) => {
-		setNewName(event.target.value)
-	}
-
-	const handleNumberChange = (event) => {
-		setNewNumber(event.target.value)
-	}
-
-	const handleFilterChange = (event) => {
-		setNewFilter(event.target.value)
-	}
-
-	const personsToShow = newFilter === ''
-		? persons
-		: persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
 	return (
 		<div>
-			<h2>Phonebook</h2>
-			<Notification message={message} errorState={errorState} />
-			<Filter value={newFilter} onChange={handleFilterChange} />
-			<h3>add a new</h3>
-			<PersonForm addName={addName} newName={newName} onNameChange={handleNameChange}
-				newNumber={newNumber} onNumberChange={handleNumberChange} />
-			<h3>Numbers</h3>
-			<Persons persons={personsToShow} setPersons={setPersons} showMessage={showMessage} />
+			<Buttons setWindowState={setWindowState} />
+			<MainContainer windowState={windowState} />
 		</div>
 	)
 }
