d507a440c6571f76f19842c4bfd66b0e324b080e
Erik Mende <emende@student.hive.fi>
2022-09-12 17:19:16 +0300

    WIP:

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,33 @@
 import signUpService from '../services/signUpService'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { AppBar, Container, Toolbar, Box, IconButton, Menu, MenuItem, Typography } from '@mui/material'
+import {
+	AppBar, Container, Toolbar, Box, IconButton, Menu, MenuItem,
+	Typography, Button, Tooltip, Avatar
+} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import { ReactComponent as Logo } from '../images/matcha_logo.svg'
+import avatar from '../images/random_picture.jpeg'
+import { useLocation } from 'react-router-dom'
+import { margin } from '@mui/system'
+
+// const Buttons = () => {
+// 	return (
+// 		<div>
+// 			<Link to="/login"><button>login</button></Link>
+// 			<Link to="/signup"><button>signup</button></Link>
+// 			<Link to="/profile"><button>profile</button></Link>
+// 			<Link to="/browse_users"><button>browse users</button></Link>
+// 			<Link to="/chat"><button>chat</button></Link>
+// 			<Link to="/phonebook"><button>phonebook</button></Link>
+// 		</div>
+// 	)
+// }
 
 
-const Buttons = () => {
-	return (
-		<div>
-			<Link to="/login"><button>login</button></Link>
-			<Link to="/signup"><button>signup</button></Link>
-			<Link to="/profile"><button>profile</button></Link>
-			<Link to="/browse_users"><button>browse users</button></Link>
-			<Link to="/chat"><button>chat</button></Link>
-			<Link to="/phonebook"><button>phonebook</button></Link>
-		</div>
-	)
-}
+
+
 
 const navbar_theme = createTheme({
 	palette: {
@@ -37,12 +46,21 @@ const NavBar = ({ user, setUser }) => {
 		setUser("")
 	}
 
-	// const pages = {'Login': '/login', 'Signup': '/signup', 'Browse Users': '/browse_users', 'Chat': '/chat', 'Phonebook': '/phonebook'}
-	const pages = ["Login", "Signup", "Browse Users", "Chat", "Phonebook"]
-	const settings = ['Profile', 'Settings', 'Log Out']
+	const pages = {
+		'Login': '/login',
+		'Signup': '/signup',
+		'Browse Users': '/browse_users',
+		'Chat': '/chat',
+		'Phonebook': '/phonebook'
+	}
+	const settings = {
+		'Profile': '/profile',
+		'Settings': '/settings',
+		'Log Out': '/logout'
+	}
 
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = useState(null);
+	const [anchorElUser, setAnchorElUser] = useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -62,9 +80,7 @@ const NavBar = ({ user, setUser }) => {
 	return (
 		<AppBar position="static" theme={navbar_theme}>
 			<Container maxWidth='xl'>
-				<Toolbar disableGutters>
-					<Logo height='50px' />
-				</Toolbar>
+
 				<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
 					<IconButton
 						size="large"
@@ -76,6 +92,16 @@ const NavBar = ({ user, setUser }) => {
 					>
 						<MenuIcon />
 					</IconButton>
+					<Toolbar disableGutters>
+						<Logo
+							height='40px'
+							sx={{
+								display: { xs: 'none', md: 'flex' },
+								mr: 1,
+								margin: '10px'
+							}} />
+					</Toolbar>
+
 					<Menu
 						id="menu-appbar"
 						anchorEl={anchorElNav}
@@ -86,17 +112,73 @@ const NavBar = ({ user, setUser }) => {
 						onClose={handleCloseNavMenu}
 						sx={{ display: { xs: 'block', md: 'none' } }}
 					>
-						{pages.map((page) => (
-							<Link to='/login'>
-								<MenuItem onClick={handleCloseNavMenu} key={page}>
-									<Typography textAlign='center' variant='h6'>{page}</Typography>
-								</MenuItem>
-							</Link>
-						))}
+						{
+							Object.keys(pages).map((page) => {
+								return (
+									<MenuItem key={page} onClick={handleCloseNavMenu} component={Link} to={pages[page]}>
+										{page}
+									</MenuItem>
+								)
+							})
+						}
 					</Menu>
 				</Box>
-				<p>User: {user} <button onClick={() => logOut()}>logout</button></p>
-				<Buttons />
+
+				<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+					<Logo
+						height='40px'
+						sx={{
+							mr: 2,
+							display: { xs: 'flex', md: 'none' },
+							mt: 45,
+						}}
+					/>
+					{
+						Object.keys(pages).map((page) => {
+							return (
+								<Button
+									key={page}
+									onClick={handleCloseNavMenu}
+									sx={{ mr: 2 }}
+									component={Link} to={pages[page]}
+								>
+									{page}
+								</Button>
+							)
+						})
+					}
+				</Box>
+				<Box sx={{ flexGrow: 0 }}>
+
+					<Menu
+						sx={{ mt: '45px' }}
+						id="menu-appbar"
+						anchorEl={anchorElUser}
+						anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+						keepMounted
+						transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+						open={Boolean(anchorElUser)}
+						onClose={handleCloseUserMenu}
+					>
+						{
+							Object.keys(settings).map((setting) => {
+								return (
+									<MenuItem key={setting} onClick={handleCloseNavMenu} component={Link} to={settings[setting]}>
+										{setting}
+									</MenuItem>
+								)
+							})
+						}
+					</Menu>
+					{/* <Tooltip title="Open settings">
+						<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+							<Avatar
+								src={avatar} />
+						</IconButton>
+					</Tooltip> */}
+				</Box>
+				{/* <p>User: {user} <button onClick={() => logOut()}>logout</button></p>
+				<Buttons /> */}
 			</Container>
 		</AppBar>
 	)
