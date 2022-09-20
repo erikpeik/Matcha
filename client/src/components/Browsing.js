1ec5fc650765ab49463a24dfd8de021b2cfe56be
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-09-20 16:27:53 +0300

    sorting and filtering started for browse results

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 // import { useDispatch } from 'react-redux'
 import {
 	Typography, Button, Paper, TextField, FormControl, FormLabel, createTheme,
-	RadioGroup, FormControlLabel, Radio, InputLabel, Select, MenuItem, TextareaAutosize,
+	RadioGroup, FormControlLabel, Radio, InputLabel, Select, MenuItem, TextareaAutosize, Box, Slider
 } from '@mui/material'
 // import { Container } from '@mui/system';
 // import { IconUserCircle } from '@tabler/icons';
@@ -15,8 +15,7 @@ import browsingService from '../services/browsingService'
 const Browsing = () => {
 	const [isLoading, setLoading] = useState(true);
 	const [users, setUsers] = useState([])
-
-	var searchCriteria = {
+	const [searchCriteria, setSearchCriteria] = useState({
 		min_age: 18,
 		max_age: 99,
 		min_fame: 0,
@@ -24,8 +23,8 @@ const Browsing = () => {
 		location: 'any',
 		sorting: 'age',
 		sort_order: 'asc',
-		amount: 25,
-	}
+		amount: 2,
+	})
 
 	useEffect(() => {
 		const getAllUsers = async (users) => {
@@ -37,16 +36,39 @@ const Browsing = () => {
 		getAllUsers()
 	}, [])
 
+	const handleAmount = async (event) => {
+		const searchValues = { ...searchCriteria, amount: event.target.value }
+		var sortedUsers = await browsingService.getSortedUsers(searchValues)
+		setSearchCriteria(searchValues)
+		setUsers(sortedUsers)
+	}
+
 	const handleSorting = async (event) => {
-		searchCriteria.sorting = event.target.value
-		var sortedUsers = await browsingService.getSortedUsers(searchCriteria)
-		if (event.target.value === 'age') {
-			sortedUsers = await sortedUsers.sort(((a, b) => (a.age > b.age) ? 1 : -1))
-			setUsers(sortedUsers)
-		} else if (event.target.value === 'user_location') {
-			sortedUsers = await sortedUsers.sort(((a, b) => (a.user_location > b.user_location) ? 1 : -1))
-			setUsers(sortedUsers)
-		}
+		const searchValues = { ...searchCriteria, sorting: event.target.value }
+		var sortedUsers = await browsingService.getSortedUsers(searchValues)
+		setSearchCriteria(searchValues)
+		setUsers(sortedUsers)
+	}
+
+	const handleSortOrder = async (event) => {
+		const searchValues = { ...searchCriteria, sort_order: event.target.value }
+		var sortedUsers = await browsingService.getSortedUsers(searchValues)
+		setSearchCriteria(searchValues)
+		setUsers(sortedUsers)
+	}
+
+	const handleAgeSlider = async (event) => {
+		const searchValues = { ...searchCriteria, min_age: event.target.value[0], max_age: event.target.value[1] }
+		var sortedUsers = await browsingService.getSortedUsers(searchValues)
+		setSearchCriteria(searchValues)
+		setUsers(sortedUsers)
+	}
+
+	const handleFameSlider = async (event) => {
+		const searchValues = { ...searchCriteria, min_fame: event.target.value[0], max_fame: event.target.value[1] }
+		var sortedUsers = await browsingService.getSortedUsers(searchValues)
+		setSearchCriteria(searchValues)
+		setUsers(sortedUsers)
 	}
 
 	if (isLoading) {
@@ -55,6 +77,36 @@ const Browsing = () => {
 		const profile_pic = require('../images/demo_profilepic.jpeg')
 		return (
 			<>
+				<FormControl fullWidth sx={{ mb: 2 }}>
+					<InputLabel id='amount'>Amount of results:</InputLabel>
+					<Select labelId='amount' id='amount' name='amount' value={searchCriteria.amount} onChange={handleAmount} required>
+						{[...Array(5).keys()].map((i) => (
+							<MenuItem value={i + 1} key={i + 1}>{i + 1}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+				<Box sx={{ width: 300 }}>
+					<InputLabel id='ageslider'>Filter by age:</InputLabel>
+					<Slider
+						min={18}
+						max={99}
+						aria-labelledby='age range'
+						value={[searchCriteria.min_age, searchCriteria.max_age]}
+						onChange={handleAgeSlider}
+						valueLabelDisplay="auto"
+						// getAriaValueText={valuetext}
+					/>
+				</Box>
+				<Box sx={{ width: 300 }}>
+					<InputLabel id='fameslider'>Filter by fame rating:</InputLabel>
+					<Slider
+						aria-labelledby='fame range'
+						value={[searchCriteria.min_fame, searchCriteria.max_fame]}
+						onChange={handleFameSlider}
+						valueLabelDisplay="auto"
+						// getAriaValueText={valuetext}
+					/>
+				</Box>
 				<FormControl sx={{ mb: 2 }}>
 					<FormLabel id='sorted_by'>Results sorted by:</FormLabel>
 					<RadioGroup row aria-labelledby='sorted_by' name='sorted_by' onChange={handleSorting}>
@@ -64,6 +116,15 @@ const Browsing = () => {
 						<FormControlLabel value='common_tags' control={<Radio />} label='Common tags' />
 					</RadioGroup>
 				</FormControl>
+				<Box>
+				<FormControl sx={{ mb: 2 }}>
+					<FormLabel id='asc_desc'></FormLabel>
+					<RadioGroup row aria-labelledby='asc_desc' name='asc_desc' onChange={handleSortOrder}>
+						<FormControlLabel value='asc' control={<Radio />} label='Ascending' />
+						<FormControlLabel value='desc' control={<Radio />} label='Descending' />
+					</RadioGroup>
+				</FormControl>
+				</Box>
 				{users.map(user =>
 					<div id="profile_container">
 						<div id="picture_container">
