ec509a53d3856070894d66f078880a5559f5abdd
Erik Mende <emende@student.hive.fi>
2022-09-20 22:16:25 +0300

    Browsing page small tweak, not finished yet

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import {
-	FormControl, FormLabel,	RadioGroup, FormControlLabel, Radio, InputLabel, Select, MenuItem, Box, Slider
+	FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, InputLabel,
+	Select, MenuItem, Box, Slider, Container, Paper
 } from '@mui/material'
 import browsingService from '../services/browsingService'
 
@@ -77,88 +78,90 @@ const Browsing = () => {
 	} else {
 		const profile_pic = require('../images/demo_profilepic.jpeg')
 		return (
-			<>
-				<FormControl fullWidth sx={{ mb: 2 }}>
-					<InputLabel id='amount'>Amount of results:</InputLabel>
-					<Select labelId='amount' id='amount' name='amount' value={searchCriteria.amount} onChange={handleAmount} required>
-						{[...Array(5).keys()].map((i) => (
-							<MenuItem value={i + 1} key={i + 1}>{i + 1}</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-				<Box sx={{ width: 300 }}>
-					<InputLabel id='ageslider'>Filter by age:</InputLabel>
-					<Slider
-						min={18}
-						max={99}
-						aria-labelledby='age range'
-						value={[searchCriteria.min_age, searchCriteria.max_age]}
-						onChange={handleAgeSlider}
-						valueLabelDisplay="auto"
+			<Container maxWidth='md' sx={{ pt: 5, pb: 5 }}>
+				<Paper elevation={10} sx={{ p: 3 }}>
+					<FormControl fullWidth sx={{ mb: 2 }}>
+						<InputLabel id='amount'>Amount of results:</InputLabel>
+						<Select labelId='amount' id='amount' name='amount' value={searchCriteria.amount} onChange={handleAmount} required>
+							{[...Array(5).keys()].map((i) => (
+								<MenuItem value={i + 1} key={i + 1}>{i + 1}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+					<Box sx={{ width: 300 }}>
+						<InputLabel id='ageslider'>Filter by age:</InputLabel>
+						<Slider
+							min={18}
+							max={120}
+							aria-labelledby='age range'
+							value={[searchCriteria.min_age, searchCriteria.max_age]}
+							onChange={handleAgeSlider}
+							valueLabelDisplay="auto"
 						// getAriaValueText={valuetext}
-					/>
-				</Box>
-				<Box sx={{ width: 300 }}>
-					<InputLabel id='fameslider'>Filter by fame rating:</InputLabel>
-					<Slider
-						aria-labelledby='fame range'
-						value={[searchCriteria.min_fame, searchCriteria.max_fame]}
-						onChange={handleFameSlider}
-						valueLabelDisplay="auto"
+						/>
+					</Box>
+					<Box sx={{ width: 300 }}>
+						<InputLabel id='fameslider'>Filter by fame rating:</InputLabel>
+						<Slider
+							aria-labelledby='fame range'
+							value={[searchCriteria.min_fame, searchCriteria.max_fame]}
+							onChange={handleFameSlider}
+							valueLabelDisplay="auto"
 						// getAriaValueText={valuetext}
-					/>
-				</Box>
-				<Box sx={{ width: 300 }}>
-					<InputLabel id='distanceslider'>Filter by distance:</InputLabel>
-					<Slider
-						min={0}
-						max={20000}
-						aria-labelledby='distance range'
-						value={[searchCriteria.min_distance, searchCriteria.max_distance]}
-						onChange={handleDistanceSlider}
-						valueLabelDisplay="auto"
+						/>
+					</Box>
+					<Box sx={{ width: 300 }}>
+						<InputLabel id='distanceslider'>Filter by distance:</InputLabel>
+						<Slider
+							min={0}
+							max={20000}
+							aria-labelledby='distance range'
+							value={[searchCriteria.min_distance, searchCriteria.max_distance]}
+							onChange={handleDistanceSlider}
+							valueLabelDisplay="auto"
 						// getAriaValueText={valuetext}
-					/>
-				</Box>
-				<FormControl sx={{ mb: 2 }}>
-					<FormLabel id='sorted_by'>Results sorted by:</FormLabel>
-					<RadioGroup row aria-labelledby='sorted_by' name='sorted_by' onChange={handleSorting}>
-						<FormControlLabel value='age' control={<Radio />} label='Age' />
-						<FormControlLabel value='user_location' control={<Radio />} label='Location' />
-						<FormControlLabel value='fame_rating' control={<Radio />} label='Fame Rating' />
-						<FormControlLabel value='common_tags' control={<Radio />} label='Common tags' />
-					</RadioGroup>
-				</FormControl>
-				<Box>
-				<FormControl sx={{ mb: 2 }}>
-					<FormLabel id='asc_desc'></FormLabel>
-					<RadioGroup row aria-labelledby='asc_desc' name='asc_desc' onChange={handleSortOrder}>
-						<FormControlLabel value='asc' control={<Radio />} label='Ascending' />
-						<FormControlLabel value='desc' control={<Radio />} label='Descending' />
-					</RadioGroup>
-				</FormControl>
-				</Box>
-				{users.map(user =>
-					<div id="profile_container">
-						<div id="picture_container">
-							<img key={user.uniqueId} alt="profile_picture" src={profile_pic} height="200px"></img>
+						/>
+					</Box>
+					<FormControl sx={{ mb: 2 }}>
+						<FormLabel id='sorted_by'>Results sorted by:</FormLabel>
+						<RadioGroup row aria-labelledby='sorted_by' name='sorted_by' onChange={handleSorting}>
+							<FormControlLabel value='age' control={<Radio />} label='Age' />
+							<FormControlLabel value='user_location' control={<Radio />} label='Location' />
+							<FormControlLabel value='fame_rating' control={<Radio />} label='Fame Rating' />
+							<FormControlLabel value='common_tags' control={<Radio />} label='Common tags' />
+						</RadioGroup>
+					</FormControl>
+					<Box>
+						<FormControl sx={{ mb: 2 }}>
+							<FormLabel id='asc_desc'></FormLabel>
+							<RadioGroup row aria-labelledby='asc_desc' name='asc_desc' onChange={handleSortOrder}>
+								<FormControlLabel value='asc' control={<Radio />} label='Ascending' />
+								<FormControlLabel value='desc' control={<Radio />} label='Descending' />
+							</RadioGroup>
+						</FormControl>
+					</Box>
+					{users.map(user =>
+						<div id="profile_container">
+							<div id="picture_container">
+								<img key={user.uniqueId} alt="profile_picture" src={profile_pic} height="200px"></img>
+							</div>
+							<div id="profile_data">
+								<h1>{user.username}</h1>
+								<h3>Fame rating: {user.fame_rating}</h3>
+								<br></br>
+								<p>First name: {user.firstname}</p>
+								<p>Last name: {user.lastname}</p>
+								<p>Gender: {user.gender}</p>
+								<p>Age: {user.age}</p>
+								<p>Sexual preference: {user.sexual_pref}</p>
+								<p>Location: {user.user_location}</p>
+								<p>Distance: {Math.floor(user.distance)} km</p>
+								<p>Biography: {user.biography}</p>
+							</div>
 						</div>
-						<div id="profile_data">
-							<h1>{user.username}</h1>
-							<h3>Fame rating: {user.fame_rating}</h3>
-							<br></br>
-							<p>First name: {user.firstname}</p>
-							<p>Last name: {user.lastname}</p>
-							<p>Gender: {user.gender}</p>
-							<p>Age: {user.age}</p>
-							<p>Sexual preference: {user.sexual_pref}</p>
-							<p>Location: {user.user_location}</p>
-							<p>Distance: {Math.floor(user.distance)} km</p>
-							<p>Biography: {user.biography}</p>
-						</div>
-					</div>
-				)}
-			</>
+					)}
+				</Paper>
+			</Container>
 		)
 	}
 }
