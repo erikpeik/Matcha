d37c58fe9dc043cffb38e7ed44d7967c29281528
Erik Mende <emende@student.hive.fi>
2022-09-23 13:01:09 +0300

    Basic styling for ChatBody

--- client/src/components/chat/ChatBody.js
+++ client/src/components/chat/ChatBody.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, Box, Paper } from '@mui/material'
 
 const ChatBody = ({ messages, user }) => {
 	return (
@@ -11,27 +11,39 @@ const ChatBody = ({ messages, user }) => {
 			{messages.map(message => {
 				if (message.name === user.user) {
 					return (
-						<div key={message.id}>
-							<p>You</p>
-							<div>
-								<p>{message.text}</p>
-							</div>
-						</div>
+						<Box key={message.id} sx={{ display: 'flex', alignItems: "flex-end", justifyContent: 'flex-end', mb: 1 }}>
+							<ul style={{margin: 0, padding: 0, width: '70%', maxWidth: '500px', marginRight: '10px'}}>
+								<li style={{listStyleType: 'none'}}>
+									<Typography sx={{mr: 0, textAlign: 'right'}}>You</Typography>
+								</li>
+								<li style={{listStyleType: 'none'}}>
+									<Paper sx={{ ml: 1, background: 'rgb(240, 246, 233)' }}>
+										<Typography sx={{ p: 1 }}>{message.text}</Typography>
+									</Paper>
+								</li>
+							</ul>
+						</Box>
 					)
 				} else {
 					return (
-						<div key={message.id}>
-						<p>{message.name}</p>
-						<div>
-							<p>{message.text}</p>
-						</div>
-					</div>
+						<Box key={message.id} sx={{ display: 'flex', alignItems: "flex-start", justifyContent: 'flex-start', mb: 1 }}>
+							<ul style={{margin: 0, padding: 0, width: '70%', maxWidth: '500px', marginRight: '10px'}}>
+								<li style={{listStyleType: 'none'}}>
+									<Typography sx={{ml: 1}}>{message.name}</Typography>
+								</li>
+								<li style={{listStyleType: 'none'}}>
+									<Paper sx={{ ml: 1, background: 'rgb(233, 240, 243)' }}>
+										<Typography sx={{ p: 1 }}>{message.text}</Typography>
+									</Paper>
+								</li>
+							</ul>
+						</Box>
 					)
 				}
 			})}
-			<div className='message_status'>
+			{/* <div className='message_status'>
 				<p>Someone is typing...</p>
-			</div>
+			</div> */}
 		</>
 	)
 }
