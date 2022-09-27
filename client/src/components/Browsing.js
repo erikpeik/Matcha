0a46b01dc700016c62801818ccbeed16f60029c7
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-09-27 10:35:53 +0300

    page numbering fixed for browsing

--- client/src/components/Browsing.js
+++ client/src/components/Browsing.js
@@ -69,6 +69,16 @@ const Browsing = () => {
 	console.log("Liked users: ", likedUsers)
 	console.log("Connected users: ", connectedUsers)
 
+	if (isLoading) {
+		return <Loader />
+	}
+
+	console.log("amount we got: ", users.length)
+	console.log("amount we wanted: ", searchCriteria.amount)
+	console.log("offset: ", searchCriteria.offset)
+	const total_results = users[0].total_results
+	const final_page = Math.ceil(total_results / searchCriteria.amount)
+
 	const handleAmount = (event) => {
 		setSearchCriteria({ ...searchCriteria, page: 1, amount: event.target.value })
 	}
@@ -97,6 +107,16 @@ const Browsing = () => {
 		setSearchCriteria({ ...searchCriteria, page: page, offset: (page - 1) * searchCriteria.amount })
 	}
 
+	const handlePageMinus = (page) => {
+		if (page > 1)
+			setSearchCriteria({ ...searchCriteria, page: page - 1, offset: (page - 1) * searchCriteria.amount })
+	}
+
+	const handlePagePlus = (page) => {
+		if (page < final_page)
+			setSearchCriteria({ ...searchCriteria, page: page + 1, offset: (page - 1) * searchCriteria.amount })
+	}
+
 	const likeUser = async (user_id) => {
 		browsingService.likeUser(user_id).then((response) => {
 			console.log(response)
@@ -111,126 +131,118 @@ const Browsing = () => {
 		setSearchCriteria({ ...searchCriteria })
 	}
 
-	if (isLoading) {
-		return <Loader />;
-	} else {
-		console.log("amount we got: ", users.length)
-		console.log("amount we wanted: ", searchCriteria.amount)
-		console.log("offset: ", searchCriteria.offset)
-		const total_results = users[0].total_results
-		const final_page = Math.ceil(total_results / searchCriteria.amount)
-		return (
-			<Container maxWidth='md' sx={{ pt: 5, pb: 5 }}>
-				<Paper elevation={10} sx={{ p: 3 }}>
-					<div className="pagination">
-						<button onClick={() => handlePageChange(1)}>First</button>
-						<button onClick={() => handlePageChange(searchCriteria.page - 1)}>&laquo;</button>
-						<>		Page {searchCriteria.page} / {final_page}		</>
-						<button onClick={() => handlePageChange(searchCriteria.page + 1)}>&raquo;</button>
-						<button onClick={() => handlePageChange(final_page)}>Last</button>
-					</div>
-					<br></br>
-					<FormControl fullWidth sx={{ mb: 2 }}>
-						<InputLabel id='amount'>Amount of results per page:</InputLabel>
-						<Select labelId='amount' id='amount' name='amount' value={searchCriteria.amount} onChange={handleAmount} required>
-							<MenuItem value={10} key={10}>{10}</MenuItem>
-							<MenuItem value={50} key={50}>{50}</MenuItem>
-							<MenuItem value={100} key={100}>{100}</MenuItem>
-							<MenuItem value={250} key={250}>{250}</MenuItem>
-							<MenuItem value={500} key={500}>{500}</MenuItem>
-						</Select>
-					</FormControl>
-					<Box sx={{ width: 300 }}>
-						<InputLabel id='ageslider'>Filter by age:</InputLabel>
-						<Slider
-							min={18}
-							max={120}
-							aria-labelledby='age range'
-							value={[searchCriteria.min_age, searchCriteria.max_age]}
-							onChange={handleAgeSlider}
-							valueLabelDisplay="auto"
-						// getAriaValueText={valuetext}
-						/>
-					</Box>
-					<Box sx={{ width: 300 }}>
-						<InputLabel id='fameslider'>Filter by fame rating:</InputLabel>
-						<Slider
-							aria-labelledby='fame range'
-							value={[searchCriteria.min_fame, searchCriteria.max_fame]}
-							onChange={handleFameSlider}
-							valueLabelDisplay="auto"
-						// getAriaValueText={valuetext}
-						/>
-					</Box>
-					<Box sx={{ width: 300 }}>
-						<InputLabel id='distanceslider'>Filter by distance:</InputLabel>
-						<Slider
-							min={0}
-							max={20000}
-							aria-labelledby='distance range'
-							value={[searchCriteria.min_distance, searchCriteria.max_distance]}
-							onChange={handleDistanceSlider}
-							valueLabelDisplay="auto"
-						// getAriaValueText={valuetext}
-						/>
-					</Box>
+	return (
+		<Container maxWidth='md' sx={{ pt: 5, pb: 5 }}>
+			<Paper elevation={10} sx={{ p: 3 }}>
+				<div className="pagination">
+					<button onClick={() => handlePageChange(1)}>First</button>
+					<button onClick={() => handlePageMinus(searchCriteria.page)}>&laquo;</button>
+					<>		Page {searchCriteria.page} / {final_page}		</>
+					<button onClick={() => handlePagePlus(searchCriteria.page)}>&raquo;</button>
+					<button onClick={() => handlePageChange(final_page)}>Last</button>
+				</div>
+				<br></br>
+				<FormControl fullWidth sx={{ mb: 2 }}>
+					<InputLabel id='amount'>Amount of results per page:</InputLabel>
+					<Select labelId='amount' id='amount' name='amount' value={searchCriteria.amount} onChange={handleAmount} required>
+						<MenuItem value={10} key={10}>{10}</MenuItem>
+						<MenuItem value={50} key={50}>{50}</MenuItem>
+						<MenuItem value={100} key={100}>{100}</MenuItem>
+						<MenuItem value={250} key={250}>{250}</MenuItem>
+						<MenuItem value={500} key={500}>{500}</MenuItem>
+					</Select>
+				</FormControl>
+				<Box sx={{ width: 300 }}>
+					<InputLabel id='ageslider'>Filter by age:</InputLabel>
+					<Slider
+						min={18}
+						max={120}
+						aria-labelledby='age range'
+						value={[searchCriteria.min_age, searchCriteria.max_age]}
+						onChange={handleAgeSlider}
+						valueLabelDisplay="auto"
+					// getAriaValueText={valuetext}
+					/>
+				</Box>
+				<Box sx={{ width: 300 }}>
+					<InputLabel id='fameslider'>Filter by fame rating:</InputLabel>
+					<Slider
+						aria-labelledby='fame range'
+						value={[searchCriteria.min_fame, searchCriteria.max_fame]}
+						onChange={handleFameSlider}
+						valueLabelDisplay="auto"
+					// getAriaValueText={valuetext}
+					/>
+				</Box>
+				<Box sx={{ width: 300 }}>
+					<InputLabel id='distanceslider'>Filter by distance:</InputLabel>
+					<Slider
+						min={0}
+						max={20000}
+						aria-labelledby='distance range'
+						value={[searchCriteria.min_distance, searchCriteria.max_distance]}
+						onChange={handleDistanceSlider}
+						valueLabelDisplay="auto"
+					// getAriaValueText={valuetext}
+					/>
+				</Box>
+				<FormControl sx={{ mb: 2 }}>
+					<FormLabel id='sorted_by'>Results sorted by:</FormLabel>
+					<RadioGroup row aria-labelledby='sorted_by' name='sorted_by' value={searchCriteria.sorting} onChange={handleSorting}>
+						<FormControlLabel value='age' control={<Radio />} label='Age' />
+						<FormControlLabel value='distance' control={<Radio />} label='Distance' />
+						<FormControlLabel value='fame_rating' control={<Radio />} label='Fame Rating' />
+						<FormControlLabel value='common_tags' control={<Radio />} label='Common tags' />
+					</RadioGroup>
+				</FormControl>
+				<Box>
 					<FormControl sx={{ mb: 2 }}>
-						<FormLabel id='sorted_by'>Results sorted by:</FormLabel>
-						<RadioGroup row aria-labelledby='sorted_by' name='sorted_by' value={searchCriteria.sorting} onChange={handleSorting}>
-							<FormControlLabel value='age' control={<Radio />} label='Age' />
-							<FormControlLabel value='distance' control={<Radio />} label='Distance' />
-							<FormControlLabel value='fame_rating' control={<Radio />} label='Fame Rating' />
-							<FormControlLabel value='common_tags' control={<Radio />} label='Common tags' />
+						<FormLabel id='asc_desc'></FormLabel>
+						<RadioGroup row aria-labelledby='asc_desc' name='asc_desc' value={searchCriteria.sort_order} onChange={handleSortOrder}>
+							<FormControlLabel value='asc' control={<Radio />} label='Ascending' />
+							<FormControlLabel value='desc' control={<Radio />} label='Descending' />
 						</RadioGroup>
 					</FormControl>
-					<Box>
-						<FormControl sx={{ mb: 2 }}>
-							<FormLabel id='asc_desc'></FormLabel>
-							<RadioGroup row aria-labelledby='asc_desc' name='asc_desc' value={searchCriteria.sort_order} onChange={handleSortOrder}>
-								<FormControlLabel value='asc' control={<Radio />} label='Ascending' />
-								<FormControlLabel value='desc' control={<Radio />} label='Descending' />
-							</RadioGroup>
-						</FormControl>
-					</Box>
-					{users.map(user => {
-						var button
-						if (connectedUsers.includes(user.id)) {
-							button = <div><Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button><Button>Connected</Button></div>
-						} else if (likedUsers.includes(user.id)) {
-							button = <Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button>
-						} else {
-							button = <Button theme={themelike} onClick={() => { likeUser(user.id) }}>Like user</Button>
-						}
-						if (!user.id) {
-							return (<></>)
-						} else
-							return (<div key={`profile_container${user.id}`} id="profile_container">
-								<div key={`picture_container${user.id}`} id="picture_container">
-									<img key={user.id} alt="profile_picture" src={user.profile_pic} height="200px"></img>
-								</div>
-								<div key={`profile_data${user.id}`} id="profile_data">
-									<h1>{user.username}</h1>
-									<h3>Fame rating: {user.fame_rating}</h3>
-									<br></br>
-									<p>First name: {user.firstname}</p>
-									<p>Last name: {user.lastname}</p>
-									<p>Gender: {user.gender}</p>
-									<p>Age: {user.age}</p>
-									<p>Sexual preference: {user.sexual_pref}</p>
-									<p>Location: {user.user_location}</p>
-									<p>Distance: {Math.floor(user.distance)} km</p>
-									<p>Biography: {user.biography}</p>
-									<p>Common tags: {user.common_tags}</p>
-									{button}
-								</div>
-							</div>
-							)
+				</Box>
+				{users.map(user => {
+					var button
+					if (connectedUsers.includes(user.id)) {
+						button = <div><Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button><Button>Connected</Button></div>
+					} else if (likedUsers.includes(user.id)) {
+						button = <Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button>
+					} else {
+						button = <Button theme={themelike} onClick={() => { likeUser(user.id) }}>Like user</Button>
 					}
-					)}
-				</Paper>
-			</Container >
-		)
-	}
+					if (!user.id) {
+						return (<></>)
+					} else
+						return (<div key={`profile_container${user.id}`} id="profile_container">
+							<div key={`picture_container${user.id}`} id="picture_container">
+								<img key={user.id} alt="profile_picture" src={user.profile_pic} height="200px"></img>
+							</div>
+							<div key={`profile_data${user.id}`} id="profile_data">
+								<h1>{user.username}</h1>
+								<h3>Fame rating: {user.fame_rating}</h3>
+								<br></br>
+								<p>First name: {user.firstname}</p>
+								<p>Last name: {user.lastname}</p>
+								<p>Gender: {user.gender}</p>
+								<p>Age: {user.age}</p>
+								<p>Sexual preference: {user.sexual_pref}</p>
+								<p>Location: {user.user_location}</p>
+								<p>Distance: {Math.floor(user.distance)} km</p>
+								<p>Biography: {user.biography}</p>
+								<p>Common tags: {user.common_tags}</p>
+								{button}
+							</div>
+						</div>
+						)
+				}
+				)}
+			</Paper>
+		</Container >
+	)
+
 }
 
 export default Browsing
