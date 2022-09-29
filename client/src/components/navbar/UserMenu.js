fdc654fbc8c109ebd1854d29c792602789853c82
Erik Mende <emende@student.hive.fi>
2022-09-29 17:00:45 +0300

    Added notification button, it doesn't do anything yet

--- client/src/components/navbar/UserMenu.js
+++ client/src/components/navbar/UserMenu.js
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Box, IconButton, Menu, MenuItem,Tooltip, Avatar
+import {
+	Box, IconButton, Menu, MenuItem, Tooltip, Avatar, Button,
 } from '@mui/material'
 import { useSelector } from 'react-redux'
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const UserMenu = ({ user }) => {
 	const [anchorElUser, setAnchorElUser] = useState(null);
+	const [anchorElNotifications, setAnchorElNotifications] = useState(null);
 	const profileData = useSelector(state => state.profile)
 	// console.log('profileData:', profileData)
 	if (profileData != null && Object.keys(profileData).length > 0)
@@ -27,33 +30,63 @@ const UserMenu = ({ user }) => {
 
 	// console.log('profile_pic:', profile_pic)
 	if (user !== undefined && user !== '' && profile_pic !== undefined) {
-		return <Box sx={{ flexGrow: 0 }}>
+		return <>
 			<Menu
 				sx={{ mt: '45px' }}
-				id="menu-appbar"
-				anchorEl={anchorElUser}
+				id="user-notifications"
+				anchorEl={anchorElNotifications}
 				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
 				keepMounted
 				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-				open={Boolean(anchorElUser)}
-				onClose={handleCloseUserMenu}
+				open={Boolean(anchorElNotifications)}
+				onClose={() => setAnchorElNotifications(null)}
 			>
-				{
-					Object.keys(settings).map(setting =>
-						<MenuItem key={setting} onClick={handleCloseUserMenu}
-							component={Link} to={settings[setting]}>
-							{setting}
-						</MenuItem>
-					)
-				}
+				{"Hello world!"}
 			</Menu>
-			<Tooltip title="Open settings">
-				<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-					<Avatar
-						src={profile_pic} />
-				</IconButton>
-			</Tooltip>
-		</Box>
+			<Button onClick={(event) => setAnchorElNotifications(event.currentTarget)}
+				sx={{
+					minWidth: 0,
+					borderRadius: '50%',
+					width: '40px',
+					height: '40px',
+					mr: 1,
+					backgroundColor: 'rgb(255, 99, 92)',
+					color: '#FFFFFF',
+					'&:hover': {
+						backgroundColor: 'rgb(255, 99, 92)',
+						boxShadow: 2
+					}
+				}}>
+				<NotificationsIcon sx={{ fontSize: '28px' }} />
+			</Button>
+			<Box sx={{ flexGrow: 0 }}>
+				<Menu
+					sx={{ mt: '45px' }}
+					id="menu-appbar"
+					anchorEl={anchorElUser}
+					anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+					keepMounted
+					transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+					open={Boolean(anchorElUser)}
+					onClose={handleCloseUserMenu}
+				>
+					{
+						Object.keys(settings).map(setting =>
+							<MenuItem key={setting} onClick={handleCloseUserMenu}
+								component={Link} to={settings[setting]}>
+								{setting}
+							</MenuItem>
+						)
+					}
+				</Menu>
+				<Tooltip title="Open settings">
+					<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+						<Avatar
+							src={profile_pic} />
+					</IconButton>
+				</Tooltip>
+			</Box>
+		</>
 	}
 }
 
