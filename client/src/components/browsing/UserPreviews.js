e1e63c6bf6123790e2ab3b332014d0a656645413
Erik Mende <emende@student.hive.fi>
2022-10-03 22:00:36 +0300

    User previews in Browsing now with different styling"

--- client/src/components/browsing/UserPreviews.js
+++ client/src/components/browsing/UserPreviews.js
@@ -1,8 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import {
-	Button, createTheme, Paper, Avatar, Box, Typography,
-	Table, TableBody, TableRow, TableCell, Grid
+	Button, createTheme, Paper, Avatar, Box, Typography, Grid
 } from '@mui/material'
 import AspectRatio from '@mui/joy/AspectRatio';
 import browsingService from '../../services/browsingService'
@@ -10,6 +9,17 @@ import { setBrowsingCriteria } from '../../reducers/browsingReducer'
 import { getUserLists } from '../../reducers/userListsReducer'
 import { changeNotification } from '../../reducers/notificationReducer'
 import { changeSeverity } from '../../reducers/severityReducer'
+import WhatshotIcon from '@mui/icons-material/Whatshot'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp'
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt'
+import BlockIcon from '@mui/icons-material/Block'
+import MaleIcon from '@mui/icons-material/Male'
+import FemaleIcon from '@mui/icons-material/Female'
+import WcIcon from '@mui/icons-material/Wc'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import TodayIcon from '@mui/icons-material/Today';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGenderless } from '@fortawesome/free-solid-svg-icons'
 
 const themelike = createTheme({
 	palette: {
@@ -59,79 +69,114 @@ const UserPreviews = ({ pageUsers, browsingCriteria }) => {
 		dispatch(setBrowsingCriteria({ ...browsingCriteria }))
 	}
 
-	const createData = (name, value) => {
-		return { name, value }
-	}
-
 	return (
 		pageUsers.map(user => {
-			var button
+			var button, gender
 			if (userLists.connected.includes(user.id)) {
-				button = <div><Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button><Button>Connected</Button></div>
+				button = <>
+					<Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button>
+					<Button>Connected</Button>
+				</>
 			} else if (userLists.liked.includes(user.id)) {
-				button = <Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}>Unlike user</Button>
+				button = <Button theme={themeunlike} onClick={() => { unlikeUser(user.id) }}><ThumbDownAltIcon sx={{ mr: 1 }} />Unlike user</Button>
 			} else {
-				button = <Button theme={themelike} onClick={() => { likeUser(user.id) }}>Like user</Button>
+				button = <Button theme={themelike} onClick={() => { likeUser(user.id) }}><ThumbUpIcon sx={{ mr: 1 }} />Like user</Button>
 			}
+
+			if (user.gender === 'male') {
+				gender = <MaleIcon sx={{
+					ml: 1,
+					fontSize: '60px',
+					color: '#89CFF0',
+					filter: 'drop-shadow(0 2px 1px rgb(31, 73, 102))'
+				}} />
+			} else if (user.gender === 'female') {
+				gender = <FemaleIcon sx={{
+					ml: 1,
+					fontSize: '60px',
+					color: '#FF7779',
+					filter: 'drop-shadow(0 2px 1px rgb(184, 84, 86))'
+				}} />
+			} else {
+				gender = <FontAwesomeIcon icon={faGenderless} style={{
+					fontSize: '50px',
+					marginLeft: '15px',
+					color: '#d859ff',
+					filter: 'drop-shadow(0 2px 1px rgb(125, 52, 148))'
+				}} />
+			}
+
 			if (!user.id) {
 				return (<div key="emptyusers"></div>)
 			} else
-				var rows = [
-					createData('Name', `${user.firstname} ${user.lastname}`),
-					createData('Fame rating', user.fame_rating),
-					createData('Gender', user.gender),
-					createData('Age', user.age),
-					createData('Sexual preference', user.sexual_pref),
-					createData('Location', user.user_location),
-					createData('Distance', `${Math.floor(user.distance)} km`),
-					createData('Common tags', user.common_tags)
-				]
-			return (
-				<Paper key={`profile_container${user.id}`} sx={{ mb: 1 }} >
-					<Grid container display='flex' sx={{ alignItems: 'center' }}>
-						<Grid item xs={4} sx={{ paddingTop: 0 }}>
-							<Box sx={{ padding: 1 }}>
-								<AspectRatio
-									component={Paper}
-									elevation={3}
-									ratio='1'
-								>
-									<Avatar
-										variant="rounded"
-										key={user.id}
-										alt="profile_picture"
-										src={user.profile_pic}
-										onClick={() => navigate(`/userprofile/${user.id}`)}
-										sx={{ width: '100%', cursor: 'pointer' }}
-									/>
-								</AspectRatio>
-							</Box>
+				return (
+					<Paper key={`profile_container${user.id}`} sx={{ mb: 1}} >
+						<Grid container display='flex' sx={{ alignItems: 'center' }}>
+							<Grid item xs={4} sx={{ paddingTop: 0 }}>
+								<Box sx={{ padding: 1 }}>
+									<AspectRatio
+										component={Paper}
+										elevation={3}
+										ratio='1'
+									>
+										<Avatar
+											variant="rounded"
+											key={user.id}
+											alt="profile_picture"
+											src={user.profile_pic}
+											onClick={() => navigate(`/userprofile/${user.id}`)}
+											sx={{ width: '100%', cursor: 'pointer' }}
+										/>
+									</AspectRatio>
+								</Box>
+							</Grid>
+							<Grid item xs={6}>
+								<Box key={`profile_data${user.id}`}>
+									<Grid display='flex' sx={{ alignItems: 'center' }}>
+										<Typography
+											variant='h2'
+											onClick={() => navigate(`/userprofile/${user.id}`)}
+											sx={{ cursor: 'pointer' }}
+										>{user.username}</Typography>
+										<Grid item>{gender}</Grid>
+									</Grid>
+									<Grid display='flex' sx={{ alignItems: 'center', mb: 1 }}>
+										<Grid display='flex'
+											sx={{
+												mr: 1, mt: '4px', border: '1px solid gray',
+												borderRadius: '10px', padding: '2px 5px',
+												backgroundColor: '#f1f1f1'
+											}}>
+											<WhatshotIcon sx={{ color: 'red' }} />
+											<Typography sx={{ fontWeight: 550 }}>{user.fame_rating}</Typography>
+										</Grid>
+										<Grid display='flex'>
+											{button}
+										</Grid>
+									</Grid>
+									<Box sx={{ ml: 1 }}>
+										<Grid display='flex' sx={{ alignItems: 'center' }}>
+											<WcIcon sx={{ color: 'gray', mr: 1 }} />
+											<Typography sx={{ fontWeight: 550 }}>{user.sexual_pref}</Typography>
+										</Grid>
+										<Grid display='flex' sx={{ alignItems: 'center' }}>
+											<LocationOnIcon sx={{ color: 'gray', mr: 1 }} />
+											<Typography sx={{ fontWeight: 550 }}>{user.user_location}</Typography>
+										</Grid>
+										<Grid display='flex' sx={{ alignItems: 'center' }}>
+											<TodayIcon sx={{ color: 'gray', mr: 1 }} />
+											<Typography sx={{ fontWeight: 550 }}>{`${user.age} years old`}</Typography>
+										</Grid>
+									</Box>
+									<Button theme={themeunlike} onClick={() => { blockUser(user.id) }} sx={{ mt: 1 }}>
+										<BlockIcon sx={{ mr: 1 }} />
+										Block user
+									</Button>
+								</Box>
+							</Grid>
 						</Grid>
-						<Grid item xs={6}>
-							<Box key={`profile_data${user.id}`}>
-								<Typography variant='h2'>{user.username}</Typography>
-								<Table>
-									<TableBody>
-										{rows.map((row) => (
-											<TableRow
-												key={row.name}
-												sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-											>
-												<TableCell component="th" scope="row">
-													{row.name}
-												</TableCell>
-												<TableCell align="right">{row.value}</TableCell>
-											</TableRow>
-										))}
-									</TableBody>
-								</Table>
-								{button}
-								<Button theme={themeunlike} onClick={() => { blockUser(user.id) }}>Block user</Button>
-							</Box>
-						</Grid>
-					</Grid>
-				</Paper>
-			)
+					</Paper>
+				)
 		}
 		))
 }
