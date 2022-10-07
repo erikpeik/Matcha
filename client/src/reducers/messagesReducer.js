0136cf183378ea0f053bbc4152d4b5b0682574e7
Erik Mende <emende@student.hive.fi>
2022-10-07 18:13:12 +0300

    Changed spaces to tabs in messagesReducer

--- client/src/reducers/messagesReducer.js
+++ client/src/reducers/messagesReducer.js
@@ -3,21 +3,21 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = []
 
 const messagesSlice = createSlice({
-    name: 'messages',
-    initialState,
-    reducers: {
-        setMessages(state, action) {
-            const content = action.payload
-            return content
-        },
-        addMessage(state, action) {
-            const content = action.payload
-            return [...state, content]
-        },
-        resetMessage(state, action) {
-            return initialState
-        }
-    }
+	name: 'messages',
+	initialState,
+	reducers: {
+		setMessages(state, action) {
+			const content = action.payload
+			return content
+		},
+		addMessage(state, action) {
+			const content = action.payload
+			return [...state, content]
+		},
+		resetMessage(state, action) {
+			return initialState
+		}
+	}
 })
 
 export const { setMessages, addMessage, resetMessage } = messagesSlice.actions
