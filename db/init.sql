8a5a3383f56501b265701ab1bc6ae6f166c965c3
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-10-10 13:12:06 +0300

    online column deleted from users

--- db/init.sql
+++ db/init.sql
@@ -41,7 +41,6 @@ CREATE TABLE IF NOT EXISTS users (
 	email VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	verified enum_yesno DEFAULT 'NO',
-	online enum_yesno DEFAULT 'NO',
 	last_connection TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
