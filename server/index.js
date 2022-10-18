d0ed61095f0bbfdeae06a2b130414d93f4093562
Erik Mende <erikpeik@outlook.com>
2022-10-19 00:05:35 +0300

    Trying to deploy to Heroku

--- server/index.js
+++ server/index.js
@@ -23,11 +23,11 @@ const socketIO = require('socket.io')(http, {
 
 const { Pool } = require('pg')
 const pool = new Pool({
-	user: 'matcha',
-	host: 'postgres-db',
-	database: 'matcha',
-	password: 'root',
-	port: 5432,
+	user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_DATABASE,
+	password: process.env.PG_PASSWORD,
+	port: process.env.PG_PORT,
 })
 
 const connectToDatabase = () => {
