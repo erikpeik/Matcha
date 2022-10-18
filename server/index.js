81350ab277d76a3d4d3c8417f1cc057f50ca7ab7
Erik Mende <erikpeik@outlook.com>
2022-10-19 00:31:54 +0300

    Database URL changed

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const socketIO = require('socket.io')(http);
 const { Pool } = require('pg')
 const pool = new Pool({
 	user: process.env.PG_USER,
-	host: process.env.PG_HOST,
+	host: process.env.DATABASE_URL,
 	database: process.env.PG_DATABASE,
 	password: process.env.PG_PASSWORD,
 	port: process.env.PG_PORT,
